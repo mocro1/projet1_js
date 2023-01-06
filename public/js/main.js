5a322547136443fda9e70695013b3edc123d46cf
mocro1 <ahmedadda2001@gmail.com>
2023-01-06 12:06:41 +0100

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+import { Personnage, perso2 } from "./module/class.js";
+import * as CLASS from "./module/class.js";
+import {test} from "./module/instances.js"
+let perso1 = new CLASS.Personnage("Bergmans","Johan");
+
+console.log(perso1,CLASS.perso2,test);
---
+++ public/js/module/class.js
@@ -0,0 +1,8 @@
+export class Personnage{
+    constructor(nom,prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
+
+export let perso2 = new Personnage("Cactus","Kbib");
